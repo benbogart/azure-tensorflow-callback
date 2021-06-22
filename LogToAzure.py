257a89375c41fbaa29780ee6a05d780a628bedfc
Ben Bogart <ben@benbogart.com>
2021-06-22 15:59:03 -0400

    add LogToAzure.py

---
+++ LogToAzure.py
@@ -0,0 +1,12 @@
+import tensorflow.keras.callbacks.Callback as Callback
+
+class LogToAzure(Callback):
+    '''Keras Callback for realtime logging to Azure'''
+    def __init__(self, run):
+        super(LogToAzure, self).__init__()
+        self.run = run
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Log all log data to Azure
+        for k, v in logs.items():
+            self.run.log(k, v)
